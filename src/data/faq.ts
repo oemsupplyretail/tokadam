856ad6cc0c42a0c0476e4e09a21a8ef11cd3ed46
idzwebclient <idz.qudani@gmail.com>
2026-08-06 21:44:20 +0800

    Update websitev1

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -1,7 +1,7 @@
 export const faqs = [
   { question: "Boleh COD ke?", answer: "Maaf, kami tidak menyediakan COD. Semua pesanan perlu dibayar terlebih dahulu dan akan diproses pada hari yang sama selepas pembayaran berjaya.", isOpen: true },
   { question: "PADOX PRO ni kebas ke?", answer: "Ya, tetapi tidak terlalu kebas sehingga menyebabkan hilang rasa nikmat. Kebas sedikit sahaja untuk kurangkan rasa sensitif dan ngilu.", isOpen: false },
-  { question: "Berapa ml dalam satu botol?", answer: "Satu botol PADOX PRO mengandungi 10ml dan boleh digunakan sebanyak 80 kali. Gunakan 1–2 calit sebelum bersama.", isOpen: false },
+  { question: "Berapa ml dalam satu botol?", answer: "Satu botol PADOX PRO mengandungi 5ml dan boleh digunakan sebanyak 30 kali. Gunakan 1–2 calit sebelum bersama.", isOpen: false },
   { question: "Perlu ke bilas selepas menggunakan PADOX PRO?", answer: "Disarankan untuk bilas dengan air supaya dapat menghilangkan sisa yang belum meresap dan memberi keselesaan pada pasangan.", isOpen: false },
   { question: "Nama PADOX PRO ada di atas bungkusan ke?", answer: "Penghantaran adalah privasi—tiada nama produk di atas bungkusan.", isOpen: false },
   { question: "Berapa lama masa diperlukan untuk nampak kesan?", answer: "Berdasarkan pengalaman pengguna, kesan boleh dilihat seawal 5 minit selepas semburan.", isOpen: false },
